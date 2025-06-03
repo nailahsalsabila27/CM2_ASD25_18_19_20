dbee9f853dc80b89b83fd79b8c33866fe3049e6b
NAJLA NURICIA LAUDY <najlalaudy@gmail.com>
2025-06-03 09:06:34 +0700

    nodeClass

---
+++ src/nodeKendaraan.java
@@ -0,0 +1,9 @@
+public class nodeKendaraan {
+    kendaraan data;
+    nodeKendaraan next;
+    nodeKendaraan(kendaraan data, nodeKendaraan next){
+        this.data=data;
+        this.next=next;
+    }
+
+}
